86224416ca353bcb253ae200e52d5d731ba28221
Subhraneel Saha <neelpapai2001@gmail.com>
2022-11-10 22:11:00 +0530

    datspor

---
+++ js/extrnal.js
@@ -0,0 +1,22 @@
+$('#header-tabs').skeletabs({ breakpoint: 0 });
+
+//cricket 
+
+$("#cricket").skeletabs({ breakpoint: 0 });
+
+//kabbdi
+
+$("#kabaddi").skeletabs({ breakpoint: 0 });
+
+//football
+
+$("#football").skeletabs({ breakpoint: 0 });
+
+//basketball
+
+$("#basketball").skeletabs({ breakpoint: 0 });
+
+// counter 
+
+$('.timer').startTimer();
+
